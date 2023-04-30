962305214f4317d30aaf0df77a78afaeac77b1ff
Fenil Gajjar <fenilgajjar2265@gmail.com>
2023-04-30 22:05:01 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -70,7 +70,7 @@ app.use((err,req,res,next) => {
     });
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     connect();
     console.log("Connected to Backend");
 });
